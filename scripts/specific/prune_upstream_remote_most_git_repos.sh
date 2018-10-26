676f9eb4ac8ebe046a0fe103f5e6139a6beca6bf
Tim Stahlhut <stahta01@gmail.com>
2018-10-25 22:11:02 -0400

    Add more set-upstream-to to "prune_upstream_remote_most_git_repos.sh"

--- scripts/specific/prune_upstream_remote_most_git_repos.sh
+++ scripts/specific/prune_upstream_remote_most_git_repos.sh
@@ -20,14 +20,20 @@ cd ../wxWidgets && pwd && \
   git remote remove wxpr && \
   git fetch --all --prune && \
   git remote add wxpr https://github.com/stahta01/wxWidgets_PR.git && \
-  git fetch wxpr staging && \
-  git branch --set-upstream-to=wxpr/staging staging && \
   git fetch wxpr wxUniversal && git checkout wxUniversal && \
   git branch --set-upstream-to=wxpr/wxUniversal wxUniversal && \
   git fetch wxpr code/fwdDeclare && git checkout code/fwdDeclare && \
   git branch --set-upstream-to=wxpr/code/fwdDeclare code/fwdDeclare && \
   git fetch wxpr code/nonPCH && git checkout code/nonPCH && \
   git branch --set-upstream-to=wxpr/code/nonPCH code/nonPCH && \
+  git fetch wxpr cmake && git checkout cmake && \
+  git branch --set-upstream-to=wxpr/cmake cmake && \
+  git fetch wxpr code/disable && git checkout code/disable && \
+  git branch --set-upstream-to=wxpr/code/disable code/disable && \
+  git fetch wxpr wxQTwin && git checkout wxQTwin && \
+  git branch --set-upstream-to=wxpr/wxQTwin wxQTwin && \
+  git fetch wxpr staging && \
+  git branch --set-upstream-to=wxpr/staging staging && \
 #cd ../wxWidgets_2_8_branch && pwd && \#
 #  git remote remove wxpr && \#
 #  git fetch --all --prune && \#
