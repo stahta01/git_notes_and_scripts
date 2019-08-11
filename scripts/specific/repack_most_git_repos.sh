8cb4f10fac204cd8e39851a5cdd717d50f8f1d86
Tim Stahlhut <stahta01@gmail.com>
2019-08-11 08:59:51 -0400

    Edit repack_most_git_repos.sh

--- scripts/specific/repack_most_git_repos.sh
+++ scripts/specific/repack_most_git_repos.sh
@@ -6,27 +6,27 @@ else
 fi
 
 cd cb_misc && pwd && \
-  git repack -d && \
+  git repack -Ad && \
 cd ../cb_projects_for_wxWidgets && pwd && \
-  git repack -d && \
+  git repack -Ad && \
 cd ../wxwidgets_build_scripts && pwd && \
-  git repack -d && \
+  git repack -Ad && \
 cd ../wxWidgets_sample_code && pwd && \
-  git repack -d && \
+  git repack -Ad && \
 ### Repos without upstream remote above this comment
 cd ../codeblocks_https_metadata && pwd && \
-  git repack -d && \
+  git repack -Ad && \
 ### Normal repos with upstream remote below this comment
 cd ../codeblocks_PCH_fixes && pwd && \
-  git repack -d && \
+  git repack -Ad && \
 cd ../wxWidgets_PR && pwd && \
-  git repack -d && \
+  git repack -Ad && \
 cd ../wxWidgets && pwd && \
-  git repack -d && \
+  git repack -Ad && \
 #cd ../wxWidgets_2_8_branch && pwd && \#
-#  git repack -d && \#
+#  git repack -Ad && \#
 cd ../wxWidgets_3_0_branch && pwd && \
-  git repack -d && \
+  git repack -Ad && \
 ### Repos with only upstream remote below this comment
 cd ../passwordsafe_git_code && pwd && \
-  git repack -d
+  git repack -Ad
